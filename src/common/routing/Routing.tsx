690c2e5cc841b4ee7f5fced9318b69508c7a71da
Margarita Gediun <margarita.gediun@gmail.com>
2026-06-13 18:10:17 +0200

    fix: routing

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -6,7 +6,7 @@ import { Login } from "@/features/auth/ui/Login/Login"
 import { Route, Routes } from "react-router"
 
 export const Path = {
-  Main: "/todolist-react",
+  Main: "/",
   Login: "login",
   NotFound: "*",
 } as const
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/todolist-react/" : "/",
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
